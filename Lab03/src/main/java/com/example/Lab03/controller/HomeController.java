95c125cc151c951e2eb2d4454257496f42768d24
Hades-KA <manhlan200604@gmail.com>
2026-02-07 10:47:23 +0700

    Update Lab03

--- Lab03/src/main/java/com/example/Lab03/controller/HomeController.java
+++ Lab03/src/main/java/com/example/Lab03/controller/HomeController.java
@@ -6,8 +6,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-    @GetMapping("/home")
+    @GetMapping({"/", "/home"})
     public String home() {
         return "index";
+    }
+
+    @GetMapping("/about")
+    public String about() {
+        return "about";
     }
 }
